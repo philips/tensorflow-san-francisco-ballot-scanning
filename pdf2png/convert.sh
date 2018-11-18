e24ac22dd84c40fcacaddd3834bd3b5c591fa6d3
Brandon Philips <brandon.philips@coreos.com>
2018-11-18 06:03:34 +0000

    convert: only convert real scans

--- pdf2png/convert.sh
+++ pdf2png/convert.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-find osv-sample-data -name '*.pdf' | while read line; do
+find osv-sample-data/2018-06-05/ballot-scans -name '*.pdf' | while read line; do
 	b=$(basename -s .pdf $line)
-	convert -density 150 $line $b.png
+	convert -rotate '-90' -density 150 $line $b.png
 done
